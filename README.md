c531e05f65b612d8e0553e74b6520eee685dfed2
Harish Urumkar <harish.urumkar@eightcap.com>
2022-09-24 19:15:21 +1000

    Deleted config files and updated the documentation

--- README.md
+++ README.md
@@ -4,42 +4,135 @@ Python script to ease (automate) the process of 2FA(MFA) authenticated session f
 
 ### Pre-requisites
   1. Latest version of AWS CLI tools installed - https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
-  2. Python v3.x or above installed
+  2. Python v3.6 or above installed
   3. Additional python modules to install
-     - `pip install coloroma` (if using Linux or Mac OS)
-     - `pip3 install coloroma` (if using Windows OS)
+     - `pip install colorama`
 
 
 ### Assumptions
-  1. AWS Profile and Credentials configured properly
-  2. MFA authentication script fails if AWS CLI is not configured properly
+This python script has been tested on Windows operating system. Although the script might work as-is on a non-windows platform, minor changes may be needed to get the script working.
 
 
-## How does the script work?
+## Structuring your config file
+  - ```config``` file should be stored under ```.aws``` directory ```(C:\Users\<your_home_directory>\.aws)```
+  - ```config``` file should contain the mfa serial arn.
+  - Sample ```config``` file
+    ```ini
+    # Default pointing to Dev Account
+    [default]
+    region = ap-southeast-2
+    output = json
+    mfa_serial = arn:aws:iam::xxxxxxxxxxxx:mfa/aaaaaaaaaaaa
+
+    # Dev account profile
+    [dev]
+    region = ap-southeast-2
+    output = json
+    mfa_serial = arn:aws:iam::xxxxxxxxxxxx:mfa/aaaaaaaaaaaa
+
+    # Test account profile
+    [acc_test]
+    region = us-east-1
+    output = json
+    mfa_serial = arn:aws:iam::xxxxxxxxxxxx:mfa/cccccccccccc
+
+    # Prod account profile
+    [prod_account]
+    region = eu-west-2
+    output = json
+    mfa_serial = arn:aws:iam::xxxxxxxxxxxx:mfa/pppppppppppp
+    ```
 
 
+## Structuring your credentials file
+  - ```credentials``` file should be stored under ```.aws``` directory ```(C:\Users\<your_home_directory>\.aws)```
+  - Each profile in the ```credentials``` file should have two sections.
+    - **_profile-name_permanent_** section where you will save your api/cli keys (aws_access_key_id and aws_secret_access_key) as permanent credentials
+    - **_profile-name_** which will have empty values. The script will overwrite this section with the STS token obtained after 2FA process.
+  - Sample ```credentials``` file
+    ```ini
+    [default_permanent]
+    aws_access_key_id = XXXXXXXXXXXXXXXXXXXX
+    aws_secret_access_key = ***********************************
 
-## How to run the script?
+    [default]
+    aws_access_key_id = 
+    aws_secret_access_key = 
+    aws_session_token = 
+
+    [dev_permanent]
+    aws_access_key_id = YYYYYYYYYYYYYYYYYYYY
+    aws_secret_access_key = ***********************************
+
+    [dev]
+    aws_access_key_id = 
+    aws_secret_access_key = 
+    aws_session_token = 
+
+    [acc_test_permanent]
+    aws_access_key_id = BBBBBBBBBBBBBBBBBBBB
+    aws_secret_access_key = ***********************************
+
+    [acc_test]
+    aws_access_key_id = 
+    aws_secret_access_key = 
+    aws_session_token = 
+
+    [prod_account_permanent]
+    aws_access_key_id = AAAAAAAAAAAAAAAAAAAA
+    aws_secret_access_key = ***********************************
+
+    [prod_account]
+    aws_access_key_id = 
+    aws_secret_access_key = 
+    aws_session_token = 
+    ```
+
+
+## Working of the script
 
 #### Preparation
   - Download and save the script in any location of your choice.
-    - For ease of use, save it under .aws directory where your config and credentials files are also available.
-  - ```config``` file under .aws directory is configured as illustrated in the section below.
-  - ```credentials``` file under .aws directory is configured as illustrated in the section below.
-  - ```temp_credentials``` file under .aws directory is configured as illustrated in the section below.
+    - For ease of use, save it under ```.aws``` directory where your config and credentials files are also available.
+  - ```config``` file under .aws directory is configured as illustrated [in this section](## Structuring your config file).
+  - ```credentials``` file under ```.aws``` directory is configured as illustrated [in this section](## Structuring your credentials file).
 
 #### Running the script
-  - Run the script using the command,
-    - ```python aws_cli.py``` (for Windows OS)
-    - ```awscli.py``` (for Linux and Mac OS)
-  - If any of the required files (config, credentials, temp_credentials) are not available under .aws directory, the script will fail with errors.
+01. Run the script using command,
+    ```python .\aws_cli.py``` (for Windows OS)
+    ```./awscli.py``` (for non-Windows OS)
+02. If any of the required files (```config```, ```credentials```) are not available under ```.aws``` directory, the script will fail with errors.
     ![Config and Credentials files missing](https://github.com/huvcodes/images-ss/blob/main/aws-cli-mfa/file_missing_errors.png)
-  - You will see the welcome message and prompt to provide the profile name.
+03. If required files are available, you will see the welcome message and prompt to provide the profile name.
     ![Welcome message](https://github.com/huvcodes/images-ss/blob/main/aws-cli-mfa/welcome_screen.png)
-  - Specify the profile name which you want to connect to. If you press enter without specifying any value, it will authenticate you with "default" settings as per your config and credentials files.
+04. Specify the profile name which you want to connect to. If you press enter without specifying any value, it will authenticate you with "default" settings as per your config and credentials files.
+    - If you provide invalid profile name, the script will fail with an error.
+      ![Invalid Profile Name](https://github.com/huvcodes/images-ss/blob/main/aws-cli-mfa/invalid_profile_name.png)
+    - If profile name and configuration is correct, the script will prompt you to provide 2FA(MFA) code.
+      ![Valid Profile Name](https://github.com/huvcodes/images-ss/blob/main/aws-cli-mfa/valid_profile_name.png)
+05. Enter the 2FA(MFA) code
+    - For valid 2FA(MFA) code, you will be successfully authenticated with AWS CLI.
+      ![Successful Authentication](https://github.com/huvcodes/images-ss/blob/main/aws-cli-mfa/successfuk_authentication.png)
+    - Profile section in ```credentials``` file will be updated with the AWS STS token
+      ```ini
+      [dev]
+      aws_access_key_id = A1B2C3D4E5F6G7H8I9J0
+      aws_secret_access_key = <random generated key>
+      aws_session_token = <random generated token>
+      ```
+      ![STS Token](https://github.com/huvcodes/images-ss/blob/main/aws-cli-mfa/sts_token.png)
+    - A copy of the credentials file will be backed up under ```C:\Users\<your_home_directory>\.aws\credentials_backup``` directory
+      - You may delete these backup files only keeping the last few (or recent copies) for your reference.
+      ![Credentials Backup](https://github.com/huvcodes/images-ss/blob/main/aws-cli-mfa/credentials_backup.png)
+06. Run few AWS CLI commands to test your 2FA authentication.
+07. After your AWS CLI session is expired, repeat steps 1-6.
 
 
-## Sample config and credentials file
-### config file
-### credentials file
- - a. pip3 install coloroma (or pip install coloroma)
+#### Session Expiration
+The session is currently set to expire after 14400 seconds (4 hours). You can change the value according to your needs.
+
+```ini
+session_time = 14400
+```
+
+
--- aws_cli.py
+++ aws_cli.py
@@ -10,20 +10,20 @@
 # Pre-Requisites:
 # -----------------
 # 1. Latest version of AWS CLI installed
-# 2. Python v3.x installed
+# 2. Python v3.6 or above installed
 # 3. Additional python modules to install
-#   a. pip3 install coloroma
+#   a. pip install colorama
 #
 #
 # Assumptions:
 # -----------------
-# 1. AWS Profile and Credentials files configured properly.
-# 2. MFA authentication script fails if AWS CLI is not configured properly.
+# This python script has been tested on Windows operating system. Although the script might work as-is on a non-windows platform, minor changes may be needed to get the script working.
 #
 
-import colorama, json, os, sys, subprocess
+import colorama, json, os, shutil, subprocess, sys
 from colorama import Fore, Style
 from configparser import ConfigParser
+from datetime import datetime
 from pathlib import Path
 from subprocess import PIPE, Popen, CalledProcessError
 
@@ -33,9 +33,10 @@ colorama.init(autoreset = True)
 # Setting the file and directory paths
 home_dir = os.path.expanduser('~')
 aws_dir = os.path.join(home_dir, '.aws')
+backup_dir = os.path.join(aws_dir, 'credentials_backup')
 config_file = os.path.join(aws_dir, 'config')
 credentials_file = os.path.join(aws_dir, 'credentials')
-temp_creds_file = os.path.join(aws_dir, 'temp_creds')
+
 
 
 # -----------------------------------------------------------------------------------------
@@ -59,10 +60,6 @@ if (not os.path.exists(config_file)):
 
 if (not os.path.exists(credentials_file)):
     print(f"{Fore.RED}{Style.BRIGHT}\nERROR : Could not locate 'credentials' file under .aws directory.\nScript aborting here !\n")
-    sys.exit(1)
-
-if (not os.path.exists(temp_creds_file)):
-    print(f"{Fore.RED}{Style.BRIGHT}\nERROR : Could not locate 'temp_creds' file under .aws directory.\nScript aborting here !\n")
     sys.exit(1)
 
 # Block to check if AWS credentials and config file exists or not - End
@@ -91,141 +88,134 @@ if(not aws_profile):
 # -----------------------------------------------------------------------------------------
 # Block to check if [profile] exists in config and credentials file - Begin
 
+# Setting value for permanent profile
+aws_profile_permanent = aws_profile + '_permanent' 
+
 config = ConfigParser()
 config.read({config_file})
 if (not config.has_section(aws_profile)):
-    print(f"\n{Fore.RED}{Style.BRIGHT}ERROR : AWS profile '{aws_profile}' is not present in the config file. Script aborting here !\n")
+    print(f"{Fore.RED}{Style.BRIGHT}\nERROR : AWS profile '{aws_profile}' is not present in the config file. Script aborting here !\n")
     sys.exit(1)
-# else:
-#    print(f"{Fore.GREEN}{Style.BRIGHT}\n'{aws_profile}' profile is present in the file [{config_file}]")
+else:
+    # print(f"{Fore.GREEN}{Style.BRIGHT}\n'{aws_profile}' profile is present in the file [{config_file}]")
+    mfa_serial_arn = config.get(aws_profile, 'mfa_serial')
+    # print(f'{Fore.YELLOW}{Style.DIM}MFA Serial ARN available in config file is --> {mfa_serial_arn}')
+    if (not mfa_serial_arn):
+        print(f"{Fore.RED}{Style.BRIGHT}\nERROR : Profile '{aws_profile}' does not contain any mfa_serial details in the config file. Script aborting here !\n")
+        sys.exit(1)
 
 
 credentials = ConfigParser()
 credentials.read({credentials_file})
-if (not credentials.has_section(aws_profile)):
-    print(f"{Fore.RED}{Style.BRIGHT}ERROR : AWS profile '{aws_profile}' is not present in the credentials file. Script aborting here !\n")
+if (not credentials.has_section(aws_profile_permanent)):
+    print(f"{Fore.RED}{Style.BRIGHT}ERROR : AWS profile (permanent) '{aws_profile_permanent}' is not present in the credentials file. Script aborting here !\n")
     sys.exit(1)
-else:
-    # print(f"{Fore.GREEN}{Style.BRIGHT}'{aws_profile}' profile is present in the file [{credentials_file}]")
-    # mfa_serial_arn = credentials[aws_profile]['mfa_serial']
-    mfa_serial_arn = credentials.get(aws_profile, 'mfa_serial')
-    print(f'{Fore.YELLOW}{Style.DIM}MFA Serial ARN available in credentials file is --> {mfa_serial_arn}')
 
-temp_credentials = ConfigParser()
-temp_credentials.read({temp_creds_file})
+if (not credentials.has_section(aws_profile)):
+    print(f"{Fore.RED}{Style.BRIGHT}ERROR : AWS profile [{aws_profile}] is not present in the credentials file. Script aborting here !\n")
+    sys.exit(1)
 
 # Block to check if [profile] exists in config and credentials file - End
 # -----------------------------------------------------------------------------------------
-
-
-
-# -----------------------------------------------------------------------------------------
-# Block to check if any existing STS session exists with specified AWS profile - Begin
-
-print(f'{Fore.YELLOW}{Style.DIM}\nChecking if any existing STS session exists with specified AWS profile')
-
-# aws sts get-caller-identity call to verify caller identity
-sts_caller_identity_cmd = f'aws sts get-caller-identity --profile {aws_profile} --output json'
-
-try:
-    sts_caller_identity_result = subprocess.Popen(sts_caller_identity_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    sts_caller_identity_output, error = sts_caller_identity_result.communicate()
-
-    if sts_caller_identity_output:
-        print(f'\n-----------------------------------------------')
-        print(f'{Fore.GREEN}{Style.DIM}CLI tool identified an existing AWS session for the profile [{aws_profile}]')
-        try:
-            sts_caller_identity_json = json.loads(sts_caller_identity_output)
-            connected_account = sts_caller_identity_json['Account']
-            connected_arn = sts_caller_identity_json['Arn']
-        except json.decoder.JSONDecodeError:
-            print(f'{Fore.RED}{Style.BRIGHT} ERROR: Error parsing the json output of sts get-caller-identity call')
-            sys.exit(1)
-
-        print(f'Connected to Account : {connected_account}')
-        print(f'Connected as : {connected_arn}')
-        print(f'-----------------------------------------------\n')
-    
-    if error:
-        print(f'{Fore.RED}{Style.BRIGHT}\nERROR: Msg --> ', error.strip())
-        sys.exit(1)
-
-except CalledProcessError as e:
-    print(f'{Fore.RED}{Style.BRIGHT}\nERROR: CalledError --> ', e.output)
-            
-# Block to check if any existing STS session exists with specified AWS profile - End
-# -----------------------------------------------------------------------------------------
-
-
+     
 
 
 # -----------------------------------------------------------------------------------------
-# Block to check if user wants to continue in same session or wants a new session - Begin
+# Block to receive MFA code and allow authentication - Begin
 
-# print(f"{Fore.BLUE}{Style.BRIGHT}Specify your AWS profile name : ", end='')
-print(f'\nDo you want to continue in the existing session or do you want to create a new AWS CLI session ?')
-print(f"\nPress [Y] or [y] to continue OR [N] or [n] to create a new session : ", end = '')
-continueFlag = input()
+session_time = 14400
+minutes, seconds = divmod(session_time, 60)
+hours, minutes = divmod(minutes, 60)
+# days, hours = divmod (hours, 24)
 
-if (continueFlag =='Y' or continueFlag =='y'):
-    print(f'\nThanks for confirming, you can continue using AWS CLI in the existing session.\nScript will abort its execution now.!')
-    sys.exit()
-elif (continueFlag =='N' or continueFlag =='n'):
-    # Reading MFA token input for new session
-    print(f'{Fore.BLUE}{Style.BRIGHT}\nProvide your valid 6-digit MFA token : ', end='')
-    mfa_token = input()
+# Reading MFA code input for new session
+print(f'{Fore.YELLOW}{Style.BRIGHT}\nProvide your valid 6-digit MFA code : ', end='')
+mfa_code = input()
 
-    # Check if MFA token provided is null or empty
-    if(not mfa_token):
-        print(f'{Fore.RED}{Style.BRIGHT}\nERROR : MFA token provided is either null or empty. Script aborting here !\n')
-        sys.exit(1)
-    else:
-        sts_command = f'aws sts get-session-token --profile {aws_profile} --token-code {mfa_token} --serial-number {mfa_serial_arn} --duration-seconds 900'
-        # print(f'\nDEBUG: STS commmand called is --> {sts_command}\n')
-
-        try:
-            sts_result = subprocess.Popen(sts_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            sts_output, error = sts_result.communicate()
-            if sts_output:
-                # print(f'\nParsing the sts call json output.\n{sts_output}')
-                try:
-                    sts_json = json.loads(sts_output)
-                    # Setting the sts-token credentials
-                    temp_credentials['temporary']['aws_access_key_id'] = sts_json['Credentials']['AccessKeyId']
-                    temp_credentials['temporary']['aws_secret_access_key'] = sts_json['Credentials']['SecretAccessKey']
-                    temp_credentials['temporary']['aws_session_token'] = sts_json['Credentials']['SessionToken']
-
-                    # Update credentials file with the sts token
-                    try:
-                        with open(temp_creds_file, "w") as file:
-                            temp_credentials.write(file)
-                        print(f"{Fore.GREEN}{Style.BRIGHT}\nCongratulations...!! You are now successfully 2FA authenticated with AWS CLI. Well done... !\n")
-                    except:
-                        print(f"{Fore.RED}{Style.BRIGHT}\nError detected while updating credentials file with the sts token. Please check !\n")
-                        sys.exit(1)
-
-                except json.decoder.JSONDecodeError:
-                    print(f'{Fore.RED}{Style.BRIGHT} ERROR: Error parsing the json output of sts get-session-token call')
-                    sys.exit(1)
-            
-            if error:
-                # print(f"{Fore.RED}{Style.BRIGHT}\nERROR: Return Code --> ", sts_result.returncode)
-                print(f"{Fore.RED}{Style.BRIGHT}\nERROR: Msg --> ", error.strip())
-                sys.exit(1)
-        
-        except CalledProcessError as e:
-            # print(f"{Fore.RED}{Style.BRIGHT}\nERROR: CalledError --> ", e.returncode)
-            print(f"{Fore.RED}{Style.BRIGHT}\nERROR: CalledError --> ", e.output)
-        except OSError as e:
-            # print(f"{Fore.RED}{Style.BRIGHT}\nERROR: OSError --> Error Number - ", e.errno)
-            print(f"{Fore.RED}{Style.BRIGHT}\nERROR: OSError --> Error Str - ", e.strerror)
-        except:
-            print(f"{Fore.RED}{Style.BRIGHT}\nERROR: --> Unhandled exception :: ", sys.exc_info()[0])
-
+# Check if MFA code provided is null or empty
+if(not mfa_code):
+    print(f'{Fore.RED}{Style.BRIGHT}\nERROR : MFA code provided is either null or empty. Script aborting here !\n')
+    sys.exit(1)
 else:
-    print(f'\n{Fore.RED}{Style.BRIGHT}Invalid response received. Script aborting now. Bye\n')
-    sys.exit()
+    # Backup the current credentials file
+    try:
+        if (not os.path.exists(backup_dir)):
+            print(f'\nCredentials backup directory does not exist. Creating the directory now')
+            # os.mkdir(os.path.join(aws_dir, 'credentials_backup'))
+            os.mkdir(backup_dir)
 
-            
-# Block to check if user wants to continue in same session or wants a new session  - End
+        backup_file_name = datetime.now().strftime("credentials_backup_%Y-%m-%d_%H%M%S")
+        backup_file = os.path.join(backup_dir, backup_file_name) 
+        shutil.copyfile(credentials_file, backup_file)
+        print(f"\n{Fore.YELLOW}{Style.BRIGHT}Credentials file backed up as '{backup_file_name}' under '[{backup_dir}]' directory")
+    except OSError as e:
+        print(f"{Fore.RED}{Style.BRIGHT}\nERROR: OSError --> Error Str - ", e.strerror)
+        sys.exit(1)
+    
+    
+    # Substituting temporary profile with permanent credentials to obtain CLI STS
+    try:
+        # print(f'\nSubstituting temporary profile with permanent credentials to obtain CLI STS')
+        credentials[aws_profile]['aws_access_key_id'] = credentials.get(aws_profile_permanent, 'aws_access_key_id')
+        credentials[aws_profile]['aws_secret_access_key'] = credentials.get(aws_profile_permanent, 'aws_secret_access_key')
+        credentials.remove_option(aws_profile, "aws_session_token")
+        with open(credentials_file, "w") as file:
+            credentials.write(file)
+    except IOError as err:
+        print(f"{Fore.YELLOW}{Style.BRIGHT}\nWARNING: Replacing '{aws_profile}' with permanent credentails may have some issue(s). Please check !")
+        print(f"{Fore.YELLOW}{Style.BRIGHT}\nWARNING: IOError --> ", e.output)
+        # sys.exit(1)
+    except:
+        print(f"{Fore.YELLOW}{Style.BRIGHT}\nWARNING: Replacing '{aws_profile}' with permanent credentails may have some issue(s). Please check !\n")
+        # sys.exit(1)
+
+
+    # Firing STS command
+    sts_command = f'aws sts get-session-token --profile {aws_profile} --token-code {mfa_code} --serial-number {mfa_serial_arn} --duration-seconds {session_time}'
+    # print(f'\nDEBUG: STS commmand called is --> {sts_command}\n')
+
+    try:
+        sts_result = subprocess.Popen(sts_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        sts_output, error = sts_result.communicate()
+        if sts_output:
+            # print(f'\nParsing the sts call json output.\n{sts_output}')
+            try:
+                sts_json = json.loads(sts_output)
+                # Setting the sts-token credentials
+                credentials[aws_profile]['aws_access_key_id'] = sts_json['Credentials']['AccessKeyId']
+                credentials[aws_profile]['aws_secret_access_key'] = sts_json['Credentials']['SecretAccessKey']
+                credentials[aws_profile]['aws_session_token'] = sts_json['Credentials']['SessionToken']
+                # session_expiry = sts_json['Credentials']['Expiration']
+
+                # Update credentials file with the sts token
+                try:
+                    with open(credentials_file, "w") as file:
+                        credentials.write(file)
+                    print(f'{Fore.GREEN}{Style.BRIGHT}\nCongratulations...!! You are now successfully 2FA authenticated with AWS CLI. !')
+                    # print(f'{Fore.GREEN}{Style.BRIGHT}The session will expire at [{session_expiry}] UTC timezone!')
+                    print (f'{Fore.GREEN}{Style.BRIGHT}The session will expire after {int(hours)} hours, {int(minutes)} minutes, and {int(seconds)} seconds')
+                    print(f'{Fore.GREEN}{Style.BRIGHT}Bye...\n')
+                except:
+                    print(f"{Fore.RED}{Style.BRIGHT}\nError detected while updating credentials file with the sts token. Please check !\n")
+                    sys.exit(1)
+
+            except json.decoder.JSONDecodeError:
+                print(f'{Fore.RED}{Style.BRIGHT} ERROR: Error parsing the json output of sts get-session-token call')
+                sys.exit(1)
+        if error:
+            # print(f"{Fore.RED}{Style.BRIGHT}\nERROR: Return Code --> ", sts_result.returncode)
+            print(f"{Fore.RED}{Style.BRIGHT}\nERROR: Msg --> ", error.strip())
+            sys.exit(1)
+    
+    except CalledProcessError as e:
+        # print(f"{Fore.RED}{Style.BRIGHT}\nERROR: CalledError --> ", e.returncode)
+        print(f"{Fore.RED}{Style.BRIGHT}\nERROR: CalledError --> ", e.output)
+    except OSError as e:
+        # print(f"{Fore.RED}{Style.BRIGHT}\nERROR: OSError --> Error Number - ", e.errno)
+        print(f"{Fore.RED}{Style.BRIGHT}\nERROR: OSError --> Error Str - ", e.strerror)
+    except:
+        print(f"{Fore.RED}{Style.BRIGHT}\nERROR: --> Unhandled exception :: ", sys.exc_info()[0])
+
+
+# Block to receive MFA code and allow authentication - End
 # -----------------------------------------------------------------------------------------
